8694e6db6ae6c43866376f8a96b398c22963774d
brunozalc <brun0zalc@hotmail.com>
2023-02-20 23:45:42 -0300

    weather slash command (wip)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+import requests
 import discord
+from discord import app_commands
 from discord.ext import commands
-from credentials import token
+from credentials import token, WEATHER_API_KEY
 
 discord.utils.setup_logging()
 
@@ -13,6 +16,11 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    try: 
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} commands')
+    except Exception as e:
+        print(f'Error syncing commands: {e}')
 
 @bot.event
 async def on_message(message):
@@ -81,4 +89,20 @@ async def multiply_command(ctx):
     except ValueError:
         await ctx.send('You did not type a number!')
 
+@bot.tree.command(name='weather', description='Checks the weather in a city')
+async def weather_command(ctx, city: str):
+    api_key = WEATHER_API_KEY
+    base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+
+    response = requests.get(base_url)
+    weather = response.json()
+
+    if response.status_code != 200:
+        print(response.status_code)
+        await ctx.response.send_message('Sorry, there was an error. Either the city does not exist or you did not type it correctly.')
+
+    else:
+        print((f'Weather in {city} is {weather["weather"][0]["description"]}. The temperature is {weather["main"]["temp"]}°C and the humidity is {weather["main"]["humidity"]}%.'))
+        await ctx.response.send_message(f'Weather in {city} is {weather["weather"][0]["description"]}. The temperature is {weather["main"]["temp"]}°C and the humidity is {weather["main"]["humidity"]}%.')
+    
 bot.run(token)
